354f2a7783737aafa572545b40a248d8239ecf24
Shubham Chaudhary <shubham.chaudhari0209@gmail.com>
2026-05-19 11:09:29 +0530

    Updated production backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-complaint-backend-amwb.onrender.com",
+  baseURL:
+    "https://ai-complaint-backend-amwb.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
